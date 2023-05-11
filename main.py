ead8faa593365763b64025aec5ebd9d3b35342bb
Himanshu Sinha <ashusinha1904@gmail.com>
2023-05-11 08:45:44 +0530

    Prototype after competition

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 from werkzeug.utils import secure_filename
-import cv2
 from pptx import Presentation
 from pptx.util import Inches
 import smtplib
@@ -22,8 +21,8 @@ import random
 
 def make_certificate(filename, template, date, title):
     i = 0
-    cid = int(time.time()*random.randint(0, 1000))
-    data = pd.read_csv(filename)
+    num = int(time.time()*random.randint(1, 1000))
+    data = pd.read_excel(filename)
     names = np.array(data['Name'])
     for name in names:
         prs = Presentation(template)
@@ -48,24 +47,59 @@ def make_certificate(filename, template, date, title):
                                 new_text = cur_text.replace(
                                     "{{DATE}}", str(date))
                                 run.text = new_text
-                    # if (not mongo.db.certificates.find_one({'name': name}) and mongo.db.certificates.find_one({'Occasion': title})):
-                        certificate = {'name': name,
-                                       'Occasion': title,
-                                       'date': date,
-                                       'cid': cid}
-                        mongo.db.certificates.insert_one(certificate)
-                    qr = qrcode.QRCode(version=None, box_size=1)
-                    data = f"https://localhost:3000/api/verify/{cid}"
-                    qr.add_data(data)
-                    qr.make(fit=True)
-                    img = qr.make_image(fill_color='black',
-                                        back_color='white')
-                    img.save(f"static/{name}qrcode.png")
-                    img_path = f"static/{name}qrcode.png"
-                    pic = slide.shapes.add_picture(
-                        img_path, 0, 0)
+                    if (shape.text.find("{{QR_HERE}}")) != -1:
+                        qr = qrcode.QRCode(version=None, box_size=4)
+                        data = f"https://verifycertificate-five.vercel.app/{num+i}"
+                        qr.add_data(data)
+                        qr.make(fit=True)
+                        img = qr.make_image(fill_color='black',
+                                            back_color='white')
+                        img.save(f"static/{name}qrcode.png")
+                        img_path = f"static/{name}qrcode.png"
+                        slide.shapes.add_picture(
+                            img_path, 0, 0)
+                        text_frame = shape.text_frame
+                        for paragraph in text_frame.paragraphs:
+                            for run in paragraph.runs:
+                                if "{{QR_HERE}}" in run.text:
+                                    # replace "NAME" with an empty string
+                                    run.text = run.text.replace(
+                                        "{{QR_HERE}}", "")
+
+                # if shape.has_text_frame:
+                #     if (shape.text.find("{{SIGN}}")) != -1:
+                #         img_path = sign
+                #         # left = Inches(shape.left)
+                #         # top = Inches(shape.top)
+                #         slide.shapes.add_picture(
+                #             img_path, 0, 0)
+                #         text_frame = shape.text_frame
+                #         for paragraph in text_frame.paragraphs:
+                #             for run in paragraph.runs:
+                #                 if "{{SIGN}}" in run.text:
+                #                     run.text = run.text.replace(
+                #                         "{{SIGN}}", "")
+
+                # qr = qrcode.QRCode(version=None, box_size=1)
+                # data = f"https://localhost:3000/api/verify/{num+i}"
+                # qr.add_data(data)
+                # qr.make(fit=True)
+                # img = qr.make_image(fill_color='black',
+                #                     back_color='white')
+                # img.save(f"static/{name}qrcode.png")
+                # img_path = f"static/{name}qrcode.png"
+                # slide.shapes.add_picture(
+                #     img_path, 0, 0)
+
+                    # pic = slide.shapes.add_picture(
+                    #     img_path, 0, 0)
                     prs.save(f"static/{name}.pptx")
                     i += 1
+        certificate = {'name': name,
+                       'Occasion': title,
+                       'date': date,
+                       'cid': num+i}
+        mongo.db.certificates.insert_one(certificate)
 
 
 def convert():
@@ -137,8 +171,9 @@ def send_mail(filename, title):
 
 
 app = Flask(__name__)
+# MongoDB URI
 app.config["MONGO_URI"] = "mongodb://localhost:27017/certificategenerator"
-mongo = PyMongo(app)
+mongo = PyMongo(app)  # Initialising MongoDB
 
 
 @app.route("/")
@@ -150,23 +185,28 @@ def home():
 def hello(cid):
     certificate = mongo.db.certificates.find_one({'cid': cid})
     if (certificate):
+        # Certificate is verified
         return render_template('verify.html')
     else:
+        # Certificate is not verified
         return render_template("not-verify.html")
 
 
 @app.route("/api/file", methods=["GET", "POST"])
 def operation():
     if request.method == "POST":
-        date = request.form.get('date')
-        title = request.form.get('title')
-        f1 = request.files['file']
+        date = request.form.get('date')  # Get the date from the form
+        title = request.form.get('title')  # Get the title from the form
+        f1 = request.files['file']  # Get the csv file from the form
+        # Get the certificate template from the form
         f2 = request.files['template']
+        # f3 = request.files['sign']
         f1.save(f'uploads/{secure_filename(f1.filename)}')
         f2.save(f'uploads/{secure_filename(f2.filename)}')
+        # f3.save(f'uploads/{secure_filename(f3.filename)}')
         make_certificate(f1.filename, f2.filename, date, title)
-        convert()
-        send_mail(f1.filename, title)
+        convert()  # For converting the output pptx file to pdf file
+        # send_mail(f1.filename, title)  # For sending mail
         return render_template("results.html")
 
 
